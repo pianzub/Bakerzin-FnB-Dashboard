b161e542d9bb6c0daa3ff46da09453a7dd612e1d
pianzub <pianzub@gmail.com>
2026-09-13 10:05:43 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.BAKERZIN_CONFIG = {
-  APP_URL: 'https://script.google.com/macros/s/AKfycbxflaVXmoUkF1LNAcZlYJ4K7ZntHFNnMjuyFamWTS4uBa7eibKGOYeWHYOFUQDcMbGIaQ/exec',
+  APP_URL: 'https://script.google.com/macros/s/AKfycbx_XFQNvMuArwhZ-GNUJGryuRSev55euJ9gIT5MTA-pxsLfYBGskTHNfRAGy1buGqk4CA/exec',
   VERSION: '2026.09.13-promo-click-v4'
 };
