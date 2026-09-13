cdb940d65781fefe44fd9685593c24fc21ab311f
pianzub <pianzub@gmail.com>
2026-09-13 10:02:32 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+window.BAKERZIN_CONFIG = {
+  APP_URL: 'https://script.google.com/macros/s/AKfycbxflaVXmoUkF1LNAcZlYJ4K7ZntHFNnMjuyFamWTS4uBa7eibKGOYeWHYOFUQDcMbGIaQ/exec',
+  VERSION: '2026.09.13-promo-click-v4'
+};
